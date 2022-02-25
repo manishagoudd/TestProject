5f55e0b8713ffe3b3c3f81de0aa3db64c6fe3fb6
Manisha <Manisha@LAPTOP-BLUDE7KD.mshome.net>
2022-02-25 08:14:24 +0530

    Initial Commit

---
+++ TestProject1/src/module-info.java
@@ -0,0 +1,46 @@
+package com.flipkart;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestProject1 {
+	
+	@Test
+	public void Testing() throws InterruptedException
+	{
+	System.out.println("flipkart started");
+	//create webpage
+	System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+	WebDriver driver= new ChromeDriver();
+	//open url
+	System.out.println("Page is open");
+	driver.get("https://www.flipkart.com/account/login");
+	Thread.sleep(3000);
+	//maximizing the browser
+	driver.manage().window().maximize();
+	Thread.sleep(3000);
+	//enter username
+	WebElement user=driver.findElement(By.cssSelector("[autocomplete] ._351hSN:nth-of-type(1) ._2IX_2-"));
+	user.sendKeys("manishaasagoni@gmail.com");
+	// enter password
+	WebElement pass=driver.findElement(By.cssSelector(".VJZDxU._2IX_2-._3mctLh"));
+	pass.sendKeys("Manisha@123");
+	//login page
+	WebElement login=driver.findElement(By.cssSelector("._2HKlqd._2KpZ6l._3AWRsL > span"));
+	login.click();
+	Thread.sleep(3000);
+	//closing the browser
+	driver.quit();
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
